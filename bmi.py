d1b8588359f83e25b94e0c8a294a3238813d8a25
Luminita Done <lumysilver@gmail.com>
2022-03-10 21:02:14 +0000

    code updated after lab 3.1

--- bmi.py
+++ bmi.py
@@ -1,10 +1,12 @@
 #declare weight and height variables
-weight = 65
-height = 180
+#after lab 3.1 I have learned hoow to use input from comand line
+#
+weight = float(input("Enter your weight"))
+height = float(input("Enter your height"))
 
-#ibm formula found at https://www.w3resource.com/python-exercises/python-basic-exercise-66.php
+#bmi formula found at https://www.w3resource.com/python-exercises/python-basic-exercise-66.php
 
-ibm = weight / (height/100)**2
+bmi = weight / (height/100)**2
 
-#print the result
-print (ibm)
+#print the result using the fortmat function and the round function as seen in 3.1 lab
+print ("Your bmi is: ", round (bmi))git 
