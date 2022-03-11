468f8cf026355b4a9bfad6ba0269d52a96dfd88a
Luminita Done <lumysilver@gmail.com>
2022-03-11 17:48:49 +0000

    update folders

---
+++ week 3/secondstring.py
@@ -0,0 +1,18 @@
+#Write a program that asks a user to input a string and outputs every second 
+# letter in reverse order
+#text: 'The quick brown fox jumps over the lazy dog.'
+
+sentence = input("Enter sentence: ")
+words = 
+
+str = "The quick brown fox jumps over the lazy dog"
+
+
+words = sentence.split('2')
+print(words[::2])
+
+
+from asyncio.format_helpers import extract_stack
+
+str(extract_stack )
+
---
+++ week 4/readme.md
@@ -0,0 +1,3 @@
+Reference to find out more about collatz: 
+If you want to learn more about collatz
+Click https://www.youtube.com/watch?v=094y1Z2wpJg&t=1 link to open resource.
