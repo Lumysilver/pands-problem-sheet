6733d42596e4bc9a1482c5a0e52bb4b2f34064c3
Luminita Done <lumysilver@gmail.com>
2022-03-12 23:47:30 +0000

    lecture week4

---
+++ week 4/lecture/testBoolian.py
@@ -0,0 +1,10 @@
+#messing with bolean
+#Author: Andrew Beatty
+
+isAlive = False
+
+print (isAlive)
+a = 99
+isEven = ( (a % 2) == 0)
+
+print ("is ", a, "even? ", isEven)
---
+++ week 4/lecture/testIf.py
@@ -0,0 +1,15 @@
+#messing with bolean
+#Author: Andrew Beatty
+
+if False:
+    #statments
+    print("condition is true")
+
+if 2 == 2:
+    print("yes the world is sane")
+
+if 2 != 3:
+    print("I hope this is not dispayed!")
+else:
+    print("2 is not equal to 2 is false")
+
