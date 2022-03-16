ab0e7c08fb8f664fe9311aac7a1192f16b827cb1
Luminita Done <lumysilver@gmail.com>
2022-03-16 21:11:47 +0000

    class staff

--- week 4/lecture/testIf.py
+++ week 4/lecture/testIf.py
@@ -4,12 +4,24 @@
 if False:
     #statments
     print("condition is true")
-
-if 2 == 2:
-    print("yes the world is sane")
+b = 3
+if b == 2:
+    print("b is equal to 2")
+else:
+    print("I hoppe this is not displayed")
 
 if 2 != 3:
     print("I hope this is not dispayed!")
 else:
     print("2 is not equal to 2 is false")
 
+#check if something is even
+aNumber = 5
+if (aNumber % 2) == 0:  #we don't necessarly need the brackets, are here only for clarity
+    print (aNumber, " is even") #another way of printing variables
+elif(aNumber % 3) == 0: #if the number is even, this will not be checked
+    print(aNumber, " is divisable by 3")
+else:
+    print(aNumber, " is not even or divisable by 3")
+
+    print("this will always be outputted")
