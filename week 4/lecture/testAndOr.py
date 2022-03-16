aac88f2ecb054124e08b69e0330c70bd019aff0c
Luminita Done <lumysilver@gmail.com>
2022-03-16 23:46:23 +0000

    update AndOr

---
+++ week 4/lecture/testAndOr.py
@@ -0,0 +1,12 @@
+#messing with and and ors
+
+number = -1
+if number >=0 and number <=100:  #we can also put brackets
+    print("insane")
+
+if(number <= 0) and (number >=100): 
+    isbad = True
+    print("stop")
+else:
+    isbad = False
+    print("go")
