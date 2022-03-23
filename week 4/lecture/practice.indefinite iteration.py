b0017b092b8bd03fe3a474e3f34dd74425d42a3c
Luminita Done <lumysilver@gmail.com>
2022-03-23 22:57:34 +0000

    practice

---
+++ week 4/lecture/practice.indefinite iteration.py
@@ -0,0 +1,9 @@
+#indefinite iteration
+
+while False:
+    print("Hello")
+
+#definite iteration runs as many timws as indicated in the code
+
+for i in a:
+    print(i)
---
+++ week 4/lecture/practice.indefinite.definite.iteration.py
@@ -0,0 +1,10 @@
+#indefinite iteration
+#while condition:
+       #code
+
+n=5
+while n > 0:
+    n = n - 1
+    print(n)
+
+#definite iteration runs as many timws as indicated in the code
