6b0bc53a9e8aa0330b67dbddef87254da71e3fe9
Luminita Done <lumysilver@gmail.com>
2022-04-01 17:52:44 +0100

    start week 5 task

---
+++ week 5/weekday.py
@@ -0,0 +1,21 @@
+#this program automaticly will tell you if 'today' is a weekday or weekend day. 
+#once you run the program, youl'll have your answer.
+
+
+# I have researched the topic and I have came across some valuable sources, such as: https://www.delftstack.com/howto/python/python-datetime-day-of-week/#:~:text=of%20the%20day.-,Use%20the%20weekday()%20Method%20to%20Get%20the%20Name%20of,0%20and%20Sunday%20is%206.
+# The calendar library can be used in Python when the day's name is required in English. It makes use of the day_name() method, which controls an array of weekdays. Monday is at the 0th position in this array.
+
+
+import datetime
+
+#I am going to give to each weekday the return message which will be displayed when the program starts
+Monday = "Yes, unfortunately today is a weekday"
+Tuesday = "Yes, unfortunately today is a weekday"
+Wednesday = "Yes, unfortunately today is a weekday"
+Thursday = "Yes, unfortunately today is a weekday"
+Friday = "Yes, unfortunately today is a weekday."
+Saturday = "It is the weekend, yay!"
+Sunday = "It is the weekend, yay!"
+
+
+
