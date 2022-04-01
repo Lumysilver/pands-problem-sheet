1b14c18366d7f90dd11214a1c4c019f54a828c14
Luminita Done <lumysilver@gmail.com>
2022-04-01 22:24:38 +0100

    weekday problem completed

--- week 5/weekday.py
+++ week 5/weekday.py
@@ -4,18 +4,29 @@
 
 # I have researched the topic and I have came across some valuable sources, such as: https://www.delftstack.com/howto/python/python-datetime-day-of-week/#:~:text=of%20the%20day.-,Use%20the%20weekday()%20Method%20to%20Get%20the%20Name%20of,0%20and%20Sunday%20is%206.
 # The calendar library can be used in Python when the day's name is required in English. It makes use of the day_name() method, which controls an array of weekdays. Monday is at the 0th position in this array.
+#I have also used as a source https://docs.python.org/3/library/datetime.html#strftime-and-strptime-behavior. 
 
+#from ast import Constant
+#from datetime import date, datetime
+#import calendar
+#from doctest import OutputChecker
+#from unittest import result
+#curr_date = date.today()
+#print(calendar.day_name[curr_date.weekday()])
+#my_dictionary_responses = {0:calendar.MONDAY,1:calendar.TUESDAY, 2:calendar.WEDNESDAY,
+#3:calendar.THURSDAY,4:calendar.FRIDAY,5:calendar.SATURDAY,6:calendar.SUNDAY}
+#if (Constant) : "Monday" <= 3
+ 
 
-import datetime
+from datetime import datetime
 
-#I am going to give to each weekday the return message which will be displayed when the program starts
-Monday = "Yes, unfortunately today is a weekday"
-Tuesday = "Yes, unfortunately today is a weekday"
-Wednesday = "Yes, unfortunately today is a weekday"
-Thursday = "Yes, unfortunately today is a weekday"
-Friday = "Yes, unfortunately today is a weekday."
-Saturday = "It is the weekend, yay!"
-Sunday = "It is the weekend, yay!"
+now = datetime.now()
 
+zi = now.strftime("%u")
+
+if (int(zi) > 5):
+    print("It is the weekend, yay!")
+else:
+    print("Yes, unfortunately today is a weekday.")    
 
 
